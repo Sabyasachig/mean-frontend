904c1daee5e86f0dcd5b8ca6a217374be2fbd719
Sabyasachi Ghosh <saghosh@dresourcesgroup.com>
2019-12-11 14:11:09 +0530

    Modified the for reactive form and image upload

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -19,11 +19,12 @@ export class PostsService{
         'http://localhost:3000/api/posts'
       )
       .pipe(map((postData) => {
-          return postData.posts.map((post: { title: any; content: any; _id: any; }) => {
+          return postData.posts.map((post: { title: any, content: any, _id: any, imagePath: any }) => {
              return {
                 title: post.title,
                 content: post.content,
-                id: post._id
+                id: post._id,
+                imagePath: post.imagePath
               };
             });
       }))
@@ -34,34 +35,57 @@ export class PostsService{
   }
 
   getPost(id: string) {
-    // return{...this.posts.find(p => p.id ===id)};
     return this.http
-      .get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/'+ id);
+      .get<{_id: string, title: string, content: string, imagePath: string}>
+      ('http://localhost:3000/api/posts/'+ id);
   }
 
   getPostUpdateListerner(){
     return this.postUpdated.asObservable();
   }
 
-  addPost(post: Post){
-    const postData: Post = {id: null, title: post.title, content: post.content};
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
-      .subscribe((res) => {
-        const postId = res.postId;
-        postData.id = postId;
-        this.posts.push(postData);
+  addPost(title: string, content: string, image: File){
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
+    this.http.post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
+      .subscribe((responseData) => {
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath
+        };
+        this.posts.push(post);
         this.postUpdated.next([...this.posts]);
         this.router.navigate(["/"]);
       });
   }
 
-  updatePost(id: string, title: string, content: string){
-    const post: Post = {id: id, title: title, content: content};
+  updatePost(id: string, title: string, content: string, image: File | string){
+    // const post: Post = {id: id, title: title, content: content, imagePath: null};
+    let postData: Post | FormData;
+    if(typeof(image) === 'object'){
+      postData = new FormData();
+      postData.append("id", id);
+      postData.append("title", title);
+      postData.append("content", content);
+      postData.append("image", image, title);
+    }else{
+      postData = {id: id, title: title, content: content, imagePath: image};
+    }
     this.http
-      .put('http://localhost:3000/api/posts/'+ id, post)
+      .put('http://localhost:3000/api/posts/'+ id, postData)
       .subscribe(response => {
         const updatePosts = [...this.posts];
-        const oldPostIndex = updatePosts.findIndex(p => p.id == post.id)
+        const oldPostIndex = updatePosts.findIndex(p => p.id == id);
+        const post = {
+          id: id,
+          title: title,
+          content: content,
+          imagePath: ""
+        };
         updatePosts[oldPostIndex] = post;
         this.posts = updatePosts;
         this.postUpdated.next([...this.posts]);
